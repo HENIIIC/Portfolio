4ab44def9a642be8fa7c43e74ae1a82e73146067
Heni Rjab <heni.rjab.1999@gmail.com>
2022-08-14 10:25:15 +0100

    add pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, } from 'react-router-dom'
+
 import NavBar from "./components/NavBar";
+import Contact from "./pages/Contact"
+import Projects from './pages/Projects'
+import Home from './pages/Home'
+import Skills from './pages/Skills'
 function App() {
   return (
-   <div>
-    <NavBar />
-    <h1>Hello</h1>
-    <button className="btn btn-success">OK</button>
-   </div>
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Route exact path= '/' component={Home}/>
+        <Route path= '/projects' component={Projects}/>
+        <Route path= '/skills' component={Skills}/>
+        <Route path= '/contact' component={Contact}/>
+        <h1>Hello</h1>
+        <button className="btn btn-success">OK</button>
+      </BrowserRouter>
+    </div>
   );
 }
 
