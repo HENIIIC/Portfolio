c5653807db63c759be3f7360cf118d3ea702d14a
Heni Rjab <heni.rjab.1999@gmail.com>
2022-08-14 12:22:54 +0100

    add content to the project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
+const PROJECTS = [
+  {
+    title: 'picing',
+    description: 'Pricing section with 3 plans',
+    url: 'https://heniiic.github.io/pricing/',
+    githubId: 'pricing'
+  },
+  {
+    title: 'Business Land',
+    description: 'Web page developed with only HTML and CSS',
+    url: 'https://heniiic.github.io/business-land/',
+    githubId: 'business-land'
+  }
+]
+
 function Projects() {
   return (
-    <div>Projects</div>
+    <div>
+      {PROJECTS.map(item => (
+        <div key={item.githubId} style={{ margin: 10, padding: 10 }}>
+          <h1>{item.title}</h1>
+          <p>{item.description}</p>
+          <a href={item.url} target='_blank' style={{textDecoration:'none'}}>Preview</a>
+          <h6>{item.githubId}</h6>
+          <hr />
+        </div>
+      ))}
+    </div>
   )
 }
 
