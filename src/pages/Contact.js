ad0bde79ec6b47fc73a21f19ee07fa9033997437
Heni Rjab <heni.rjab.1999@gmail.com>
2022-08-20 10:02:58 +0100

    add content to the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 function Contact() {
+  const contactInformations = {
+    email: 'heni.rjab.1999@gmail.com' ,
+    phoneNumber : '+216 24 582 712',
+    address : {
+      city : 'Sfax',
+      country : 'Tunisia',
+      street : 'route sidi mansour klm 5',
+      postalCode : 3011 ,
+    }
+  }
   return (
-    <div>Contact</div>
+    <div>
+      <p> <i class="bi bi-envelope"></i>  {contactInformations.email} </p>
+      <p> <i class="bi bi-telephone"></i> {contactInformations.phoneNumber} </p>
+      <p> <i class="bi bi-geo-alt"></i> {contactInformations.address.street} ; {contactInformations.address.postalCode} {contactInformations.address.city} ; {contactInformations.address.country} </p>
+    </div>
   )
 }
 
